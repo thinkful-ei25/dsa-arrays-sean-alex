435ab752413f41f663b55933ce39464440fc1fc4
Sean Nealon <signalflowsean@gmail.com>
2018-12-12 15:12:28 -0500

    alex wants to get his hands dirty!

--- array-drills2.js
+++ array-drills2.js
@@ -1,2 +1,32 @@
-console.log(((str = "tauhida parveen") => str.replace(/ /g, () => "%20"))());  
- 
+//console.log(((str = "tauhida parveen") => str.replace(/ /g, () => "%20"))());
+// console.log(((str = "www.thinkful.com /tauh ida parv een") => str.replace(/ /g, () => "%20"))());  
+
+function URLify(str){ 
+    let URLified =""; 
+    for (let i = 0; i < str.length; i++){ 
+        if (str[i] === " "){ 
+            URLified +="%20"
+        }
+        else { 
+            URLified += str[i]
+        }
+    }
+    return URLified; 
+}
+// let str = "tauhida parveen"; 
+let str = "www.thinkful.com /tauh ida parv een"
+let URLified = URLify(str); 
+console.log(URLified); 
+
+function filter(arr = [2, 2, 4, 7], num = 5){ 
+    let newArray= []; 
+    for (let i = 0; i < arr.length; i++){ 
+        if (arr[i] > num){
+            newArray.push(arr[i]);
+        }
+       
+    }
+    return newArray; 
+}
+
+console.log(filter()); 
