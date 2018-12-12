b6aa94e8d7dc4e340a0354c156bf1e4271b9dd2e
talexcrowell <talexcrowell@gmail.com>
2018-12-12 13:16:04 -0800

    finished through mergeArray

--- array-drills2.js
+++ array-drills2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //console.log(((str = "tauhida parveen") => str.replace(/ /g, () => "%20"))());
 // console.log(((str = "www.thinkful.com /tauh ida parv een") => str.replace(/ /g, () => "%20"))());  
 
@@ -30,3 +32,45 @@ function filter(arr = [2, 2, 4, 7], num = 5){
 }
 
 console.log(filter()); 
+
+function maxSum(arr){
+    let total=0;
+    let max=0;
+    for(let i=0; i < arr.length; i++){
+        let elem = arr[i];
+        total= Math.max(0, total + elem);
+        max = Math.max(max, total);
+    }
+    return max;
+}
+
+console.log(maxSum([4,6,-3,5,-2,1]));
+
+function mergeArr(arr1, arr2){
+    let newArr = [];
+    let i=0;
+    let j=0;
+    let k=0;
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]<arr2[j]){
+            newArr[k++]=arr1[i++]; 
+        } else {
+            newArr[k++]=arr2[j++];
+        }
+    }
+
+    while (i < arr1.length){
+        newArr[k++]=arr1[i++]; 
+    }
+
+    while (j < arr2.length){
+        newArr[k++]=arr2[j++];
+    }
+
+    return newArr;
+}
+
+console.log(mergeArr([1, 3, 6, 8, 11],[2, 3, 5, 8, 9, 10]));
+
+
+
